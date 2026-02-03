04e8c969599f98c0af15171e04b3c5fa8ab19380
deepikanellithirtha <deepikanellithirtha2002@gmail.com>
2026-02-03 21:31:43 +0530

    hero section image changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-/*export const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "Deepika | Full Stack Developer & Software Engineer",
   description: "Portfolio of Deepika, a Software Engineer specializing in Java, SQL, and Web Design. Explore my projects in medical imaging and web development.",
   openGraph: {
@@ -22,7 +22,7 @@ const geistMono = Geist_Mono({
     siteName: "Deepika Portfolio",
     images: [
       {
-        url: "https://i.postimg.cc/DzBC6JKs/1743681579358-1.jpg", 
+        url: "https://i.postimg.cc/4xvTJ808/Deepika-Photo.jpg", 
         width: 1200,
         height: 630,
         alt: "Deepika Portfolio Preview",
@@ -31,8 +31,8 @@ const geistMono = Geist_Mono({
     locale: "en_US",
     type: "website",
   },
-};*/
-
+};
+/*
 export const metadata: Metadata = {
   title: "Deepika | Full Stack Developer & Software Engineer",
   description: "Portfolio of Deepika, a Software Engineer specializing in Java, SQL, and Web Design.",
@@ -51,7 +51,7 @@ export const metadata: Metadata = {
     ],
     type: "website",
   },
-};
+};*/
 
 export default function RootLayout({
   children,
